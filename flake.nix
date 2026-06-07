2b3697c219e67a65225cc3026a481b568bb153e6
DestinyofYeet <ole@ole.blue>
2026-06-07 22:48:43 +0200

    feat(ci): adds workflows

--- flake.nix
+++ flake.nix
@@ -4,59 +4,62 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     { self, ... }@inputs:
-    let
-      overlays = [ (import inputs.rust-overlay) ];
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        inherit overlays;
-      };
-
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        nativeBuildInputs = import ./nix/buildInputs.nix { inherit pkgs inputs; };
-
-        # uncomment this is you get some kind of ssl error, usually on anything networking related using reqwest
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-
-        # needed env variables
-        DATABASE_URL = "./tmp/database.db";
-        CSS_ASSET_DIR = "/src/client/assets/css";
-      };
-
-      packages.x86_64-linux = {
-        default = pkgs.callPackage ./nix/pkg.nix { flake = self; };
-      };
-
-      formatter.x86_64-linux =
-        let
-          script = pkgs.writeShellScriptBin "fmt" ''
-            set -euo pipefail
-
-            dx fmt
-          '';
-        in
-        pkgs.symlinkJoin {
-          name = "fmt";
-          paths = [ script ] ++ (import ./nix/buildInputs.nix { inherit pkgs inputs; });
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/fmt --prefix PATH : $out/bin";
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import inputs.rust-overlay) ];
+        pkgs = import inputs.nixpkgs {
+          inherit overlays system;
         };
 
-      checks.x86_64-linux = {
-        formatter = pkgs.stdenv.mkDerivation {
-          name = "dx-fmt-check";
-          src = ./.;
+      in
+      {
+        devShells.default = pkgs.mkShell {
           nativeBuildInputs = import ./nix/buildInputs.nix { inherit pkgs inputs; };
-          buildPhase = ''
-            dx fmt -c
-          '';
+
+          # uncomment this is you get some kind of ssl error, usually on anything networking related using reqwest
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+          # needed env variables
+          DATABASE_URL = "./tmp/database.db";
+          CSS_ASSET_DIR = "/src/client/assets/css";
         };
-      };
-    };
+
+        packages = {
+          default = pkgs.callPackage ./nix/pkg.nix { flake = self; };
+        };
+
+        formatter =
+          let
+            script = pkgs.writeShellScriptBin "fmt" ''
+              set -euo pipefail
+
+              dx fmt
+            '';
+          in
+          pkgs.symlinkJoin {
+            name = "fmt";
+            paths = [ script ] ++ (import ./nix/buildInputs.nix { inherit pkgs inputs; });
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = "wrapProgram $out/bin/fmt --prefix PATH : $out/bin";
+          };
+
+        checks = {
+          formatter = pkgs.stdenv.mkDerivation {
+            name = "dx-fmt-check";
+            src = ./.;
+            nativeBuildInputs = import ./nix/buildInputs.nix { inherit pkgs inputs; };
+            buildPhase = ''
+              dx fmt -c
+              touch "$out"
+            '';
+          };
+        };
+      }
+    );
 }
